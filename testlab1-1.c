468f5aa4b36967b88f4d6f3eed4cd73cf13e5f3e
Dmitry Baksheev <d.baksheev@g.nsu.ru>
2024-02-12 21:57:58 +0700

    testlab1-1: revisit, add big test with 1-char sample

--- testlab1-1.c
+++ testlab1-1.c
@@ -142,24 +142,28 @@ static int checkerBig(void)
     }
 }
 
+static int NoSpaceFor(FILE *in) {
+    printf("can't create in.txt. No space on disk?\n");
+    if (in) {
+        fclose(in);
+    }
+    return -1;
+}
+
 static int FeedBig2(void)
 {
-    FILE *const in = fopen("in.txt", "w+");
-    int i, err;
-    DWORD t;
+    FILE *const in = fopen("in.txt", "w");
     if (!in) {
-        printf("can't create in.txt. No space on disk?\n");
-        return -1;
+        return NoSpaceFor(in);
     }
     printf("Creating large text... ");
     fflush(stdout);
-    t = GetTickCount();
-    for (i = 0, err = 0; err >= 0 && i < 1000000; ++i) {
+    DWORD ms = GetTickCount();
+    for (int i = 0; i < 1000000; ++i) {
+        int err;
         if (i == 0) {
             err = fputs("0123456789abcdef\n", in); // hash 11589501
-            continue;
-        }
-        if (i % 10001 == 0) {
+        } else if (i % 10001 == 0) {
             err = fputs("0123456789-bcdef ", in); // no match
         } else if (i % 500000 == 0) {
             err = fputs("---3123456789abcdef--- ", in); // h-match, 0-mismatch
@@ -172,75 +176,65 @@ static int FeedBig2(void)
         } else {
             err = fputs("0123456789abcdez ", in);
         }
+        if (err == EOF) {
+            return NoSpaceFor(in);
+        }
     }
-    if (err < 0) {
-        printf("can't create in.txt. No space on disk?\n");
-        fclose(in);
-        return -1;
-    }
-    t = RoundUptoThousand(GetTickCount() - t);
-    printf("done in T=%u seconds. Starting exe with timeout 2*T... ", (unsigned)t/1000);
-    LabTimeout = (int)t*2;
+    fclose(in);
+    ms = GetTickCount() - ms;
+    printf("done in T=%u ms. Starting exe with timeout 10*T... ", ms);
     fflush(stdout);
+    LabTimeout = 10 * ms;
     return 0;
 }
 
 static int CheckBig2(void)
 {
-    FILE *const out = fopen("out.txt", "r");
-    unsigned i, passed = 1;
+    testN++;
+    FILE *out = fopen("out.txt", "r");
+    if (!out) {
+        printf("can't open out.txt\n");
+        return -1;
+    }
     const int bigOut[] = {
         11589501, 8666637, 12133303, 12133304, 13866639, 13866640, 13866641,
     };
-    if (!out) {
-        printf("can't open out.txt\n");
-        testN++;
-        return -1;
-    }
-    for (i = 0; i < sizeof(bigOut)/sizeof(bigOut[0]); i++) {
+    for (int i = 0; i < (int)(sizeof(bigOut)/sizeof(bigOut[0])); i++) {
         int n;
         if (ScanInt(out, &n) != Pass) {
-            passed = 0;
-            break;
-        } else if (bigOut[i] != n) {
-            passed = 0;
-            printf("wrong output -- ");
-            break;
+            goto wrong_output;
+        }
+        if (bigOut[i] != n) {
+            goto wrong_output;
         }
     }
-    if (passed) {
-        passed = !HaveGarbageAtTheEnd(out);
+    if (HaveGarbageAtTheEnd(out)) {
+        goto wrong_output;
     }
     fclose(out);
-    if (passed) {
-        printf("PASSED\n");
-        testN++;
-        return 0;
-    } else {
-        printf("FAILED\n");
-        testN++;
-        return 1;
-    }
+    printf("PASSED\n");
+    return 0;
+
+wrong_output:
+    fclose(out);
+    printf("wrong output -- FAILED\n");
+    return 1;
 }
 
 static int FeedBig3(void)
 {
-    FILE *const in = fopen("in.txt", "w+");
-    int i, err;
-    DWORD t;
+    FILE *in = fopen("in.txt", "w");
     if (!in) {
-        printf("can't create in.txt. No space on disk?\n");
-        return -1;
+        return NoSpaceFor(in);
     }
     printf("Creating large text... ");
     fflush(stdout);
-    t = GetTickCount();
-    for (i = 0, err = 0; err >= 0 && i < 1000000; ++i) {
+    DWORD ms = GetTickCount();
+    for (int i = 0; i < 1000000; ++i) {
+        int err;
         if (i == 0) {
             err = fputs("01\n", in); // hash 3
-            continue;
-        }
-        if (i % 100001 == 0) {
+        } else if (i % 100001 == 0) {
             err = fputs("02468acegikmoqsuwy ", in); // no match
         } else if (i % 500000 == 0) {
             err = fputs("---31aaaaaaaaaaaaaa--- ", in); // h-match, 0-mismatch
@@ -249,55 +243,111 @@ static int FeedBig3(void)
         } else {
             err = fputs("02468acegikmoqsuwy ", in);
         }
+        if (err == EOF) {
+            return NoSpaceFor(in);
+        }
     }
-    if (err < 0) {
-        printf("can't create in.txt. No space on disk?\n");
-        fclose(in);
-        return -1;
-    }
-    t = RoundUptoThousand(GetTickCount() - t);
-    printf("done in T=%u seconds. Starting exe with timeout 2*T... ", (unsigned)t/1000);
-    LabTimeout = (int)t*2;
+    fclose(in);
+    ms = GetTickCount() - ms;
+    printf("done in T=%u ms. Starting exe with timeout 10*T... ", ms);
     fflush(stdout);
+    LabTimeout = 10 * ms;
     return 0;
 }
 
 static int CheckBig3(void)
 {
-    FILE *const out = fopen("out.txt", "r");
-    unsigned i, passed = 1;
+    testN++;
+    FILE *out = fopen("out.txt", "r");
+    if (!out) {
+        printf("can't open out.txt\n");
+        return -1;
+    }
     const int bigOut[] = {
         3, 9499985, 13299989, 13299990,
     };
-    if (!out) {
-        printf("can't open out.txt\n");
-        testN++;
-        return -1;
-    }
-    for (i = 0; i < sizeof(bigOut)/sizeof(bigOut[0]); i++) {
+    for (int i = 0; i < (int)(sizeof(bigOut)/sizeof(bigOut[0])); i++) {
         int n;
         if (ScanInt(out, &n) != Pass) {
-            passed = 0;
-            break;
-        } else if (bigOut[i] != n) {
-            passed = 0;
-            printf("wrong output -- ");
-            break;
+            goto wrong_output;
+        }
+        if (bigOut[i] != n) {
+            goto wrong_output;
         }
     }
-    if (passed) {
-        passed = !HaveGarbageAtTheEnd(out);
+    if (HaveGarbageAtTheEnd(out)) {
+        goto wrong_output;
     }
     fclose(out);
-    if (passed) {
-        printf("PASSED\n");
-        testN++;
-        return 0;
-    } else {
-        printf("FAILED\n");
-        testN++;
-        return 1;
+    printf("PASSED\n");
+    return 0;
+
+wrong_output:
+    printf("wrong output -- FAILED\n");
+    fclose(out);
+    return 1;
+}
+
+static int big4Loc1 =      123;
+static int big4Loc2 =    45678;
+static int big4Loc3 = 12345678;
+
+static int FeedBig4(void)
+{
+    FILE *in = fopen("in.txt", "w+");
+    if (!in) {
+        return NoSpaceFor(in);
     }
+    printf("Creating large text... ");
+    fflush(stdout);
+    DWORD ms = GetTickCount();
+    if (fputs("a\n", in) == EOF) {
+        return NoSpaceFor(in);
+    }
+    for (int i = 1; i <= big4Loc3; ++i) {
+        int err;
+        if (i == big4Loc1 || i == big4Loc2 || i == big4Loc3) {
+            err = fputc('d', in); // match
+        } else {
+            err = fputc('b', in); // mismatch
+        }
+        if (err == EOF) {
+            return NoSpaceFor(in);
+        }
+    }
+    fclose(in);
+    ms = GetTickCount() - ms;
+    printf("done in T=%u ms. Starting exe with timeout 10*T... ", ms);
+    fflush(stdout);
+    LabTimeout = 10 * ms;
+    return 0;
+}
+
+static int CheckBig4(void)
+{
+    testN++;
+    FILE *const out = fopen("out.txt", "r");
+    if (!out) {
+        printf("can't open out.txt\n");
+        return -1;
+    }
+    int hash, loc1, loc2, loc3;
+    if (4 != fscanf(out, "%i%i%i%i", &hash, &loc1, &loc2, &loc3)) {
+        goto wrong_output;
+    }
+    if (hash != 1 || loc1 != big4Loc1 || loc2 != big4Loc2 || loc3 != big4Loc3) {
+        goto wrong_output;
+    }
+    if (HaveGarbageAtTheEnd(out)) {
+        goto wrong_output;
+    }
+    printf("PASSED\n");
+    return 0;
+
+wrong_output:
+    printf("wrong output -- FAILED\n");
+    fclose(out);
+    return 1;
 }
 
 const TLabTest LabTests[] = {
@@ -322,6 +372,7 @@ const TLabTest LabTests[] = {
     {feederBig, checkerBig},
     {FeedBig2, CheckBig2},
     {FeedBig3, CheckBig3},
+    {FeedBig4, CheckBig4},
 };
 
 TLabTest GetLabTest(int testIdx) {
